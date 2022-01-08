b54b3331181121b5120f313c947959d150aa1047
Jonah Broyer <jkbsk8er@yahoo.com>
2022-01-07 18:31:53 -0800

    Region: Test to_s

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Region, type: :model do
     expect(region).to respond_to(:name)
   end
 
+  it "has a string representation that is its name" do
+    region = Region.new(name: 'Mt. Hood')
+    result = region.to_s
+    expect(result).to eq('Mt. Hood')
+  end
+
 end
