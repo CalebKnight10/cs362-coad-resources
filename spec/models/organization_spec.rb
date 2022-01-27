c2eba299810cac53a924f10b6daebc37731e355a
Jonah Broyer <jkbsk8er@yahoo.com>
2022-01-27 15:37:19 -0800

    Created a let block for Organization References #11

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  let :organization {
+    organization = Organization.new(
+      status: "Fake Status",
+      phone: "555-123-4567",
+      email: "fake@example.com",
+      description: "Fake Description",
+      rejection_reason: "Fake Reason",
+      liability_insurance: "Fake Insurance",
+      primary_name: "Fake First Name",
+      secondary_name: "Fake Second Name",
+      secondary_phone: "555-555-5432",
+      title: "Fake Title",
+      transportation: "Fake Car")
+  }
+
+
+  describe "validations" do
+
+    it "is invalid without an email" do
+      expect(organization).to be_invalid
+    end
+
+  end
 end
