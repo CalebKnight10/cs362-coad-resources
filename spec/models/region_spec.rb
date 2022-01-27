fa123f729ffc07faae1442387175efacd98d38c6
Caleb Knight <knightca@oregonstate.edu>
2022-01-27 11:53:16 -0800

    Began working on validating length of name

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Region, type: :model do
   describe "uniqueness" do 
     it "is not case senstive" do
       name = 'Fake Name'
-      region = Region.create(name: name)
+      region = Region.create!(name: name)
       region_two = Region.new(name: 'Unique Name')
       expect(region).to be_valid
       region_two = region.dup
@@ -47,4 +47,23 @@ RSpec.describe Region, type: :model do
     end
   end
 
+  describe "length of name" do
+    it "can't be blank" do
+      name = 'Fake Region'
+      region = Region.create!(name: name)
+      expect(region).to be_valid
+      region.name = ''
+      expect(region).to_not be_valid
+    end
+    it "can't have length greater than 255" do
+      # name = 'Fake Region'
+      # region = Region.create!(name: name)
+      # expect(region).to be_valid
+      expect(region).to 
+       validate_length_of(:name).
+       is_at_least(1). is_at_most(255)
+      #       region.name << 256
+      # expect(region).to be_valid
+    end
+  end
 end
