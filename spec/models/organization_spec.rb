3def2973c357f14047838ba43dd647d9489011a4
Jonah Broyer <jkbsk8er@yahoo.com>
2022-02-03 17:23:38 -0800

    Added tests for validation References #14

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,7 +6,34 @@ RSpec.describe Organization, type: :model do
 
   describe "validations" do
     it "is invalid without an email" do
-      expect(organization).to be_invalid
+      expect(organization).to be_valid
+    end
+    it "is invalid without an name" do
+      expect(organization).to be_valid
+    end
+    it "is invalid without an phone number" do
+      expect(organization).to be_valid
+    end
+    it "is invalid without a status" do
+      expect(organization).to be_valid
+    end
+    it "is invalid without a primary name" do
+      expect(organization).to be_valid
+    end
+    it "is invalid without a secondary name" do
+      expect(organization).to be_valid
+    end
+    it "is invalid without a seconday phone number" do
+      expect(organization).to be_valid
+    end
+    it "cannot have an email length greater than 255" do
+      expect(organization).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+    end
+    it "cannot have a name length greater than 255" do
+      expect(organization).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+    it "cannot have a description length greater than 1020" do
+      expect(organization).to validate_length_of(:description).is_at_most(1020).on(:create)
     end
   end
 end
