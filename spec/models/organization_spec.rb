1dfb9b02050073107dc5814532926b8a9dd601f5
Caleb Knight <knightca@oregonstate.edu>
2022-02-06 12:43:04 -0800

    #15 Resource Category Built factory and tests for resource_category

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,25 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-<<<<<<< HEAD
-  let (:organization) {
-    organization = Organization.new(
-      status: "Fake Status",
-      phone: "555-123-4567",
-      email: "fake@example.com",
-      description: "Fake Description",
-      rejection_reason: "Fake Reason",
-      liability_insurance: "Fake Insurance",
-      primary_name: "Fake First Name",
-      secondary_name: "Fake Second Name",
-      secondary_phone: "555-555-5432",
-      title: "Fake Title",
-      transportation: "Fake Car")
-  }
-
-=======
   let(:organization) { build(:organization) }
->>>>>>> 84de22ab6ceb6856369736515dd8161a9fbcf1c6
 
   describe "validations" do
     it "is invalid without an email" do
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,53 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let(:resource_category) { build(:resource_category) }
+
+  describe "validations" do 
+    it "is invalid without a name" do 
+      expect(resource_category).to be_valid
+    end
+    it "is invalid with a name greater than 255 char" do
+      expect(resource_category).to be_valid
+    end
+  end
+
+
+  describe "associations" do
+    it "has many tickets" do 
+      expect(resource_category).to have_many(:tickets)
+    end
+  end
+
+  
+  describe "methods" do
+    describe "unspecified" do
+      it "returns a resource category with the name 'Unspecified'" do
+        resource_category = build(:resource_category, name: 'Unspecified')
+        expect(resource_category.name).to eq('Unspecified')
+      end
+    end
+    describe "activate" do
+      it "makes a region category active" do
+        expect(resource_category.activate).to be_truthy
+      end
+    end
+    describe "deactivate" do 
+      it "makes a region category unactive" do 
+        expect(resource_category.deactivate).to be_truthy
+      end
+    end
+    describe "inactive" do 
+      it "is an inactive resource category" do 
+        resource_category.active = true
+        expect(resource_category.inactive?).to eq(false)
+      end
+    end
+    describe "to_s" do
+      it "has a string representation that is its name" do 
+        name = resource_category.name
+        expect(resource_category.to_s).to eq(name)
+      end
+    end
+  end
 end
