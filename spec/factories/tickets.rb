4b99912e8d71e2ed42f5c50558c9396ae56cee9d
Caleb Knight <knightca@oregonstate.edu>
2022-02-06 14:11:36 -0800

    #16 Got alot of ticket under test. Struggling with name/id.  Have more to do.

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :ticket do
     name { "Fake Ticket Name" }
-    phone { "123-456-7891" }
-    region_id { "Fake Region ID" }
-    resource_category_id { "Fake Resource Category ID" }
+    phone { "123-456-7890" }
+    region_id { "1" }
+    resource_category_id { "1" }
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  let(:ticket) { build(:ticket) }
+
+  describe "validations" do
+    it "is invalid without a name" do
+      expect(ticket).to be_valid
+    end
+    it "is invalid without a phone number" do 
+      expect(ticket).to validate_presence_of(:phone)
+    end
+    it "is invalid without a region ID" do
+      expect(ticket).to validate_presence_of(:region_id)
+    end
+    it "is invalid without a resource category ID" do 
+      expect(ticket).to validate_presence_of(:resource_category_id)
+    end
+  end
+
+  describe "methods" do 
+    describe "open?" do 
+      it "is an open ticket?" do 
+        ticket.closed
+        expect(ticket.open?).to eq(true)
+      end
+    end
+    describe "captured?" do 
+      it "has a present organization" do 
+        ticket.organization.present?
+        expect(ticket.captured?).to be(false)
+      end
+    end
+    let(:ticket) { build(:ticket, name:"Ticket ") }
+    describe "to_s" do 
+      it "has a string representation that is its name" do 
+        name = ticket.name
+        expect(ticket.to_s).to eq(name)
+      end
+    end
+  end
 end
