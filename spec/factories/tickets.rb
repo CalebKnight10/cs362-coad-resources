e4249fa20d735e5e8355f6393939c1cb5c02504e
Caleb Knight <knightca@oregonstate.edu>
2022-02-12 15:36:36 -0800

    Got more tickets under test... failing woohoo

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
-  factory :ticket do
-    name { "Fake Ticket Name" }
-    phone { "555-123-4567" }
-    region_id { 1 }
-    resource_category_id { 1 }
-    description { "Fake Description" }
-  end
+  # factory :ticket do
+  #   name { "Fake Ticket Name" }
+  #   phone { "+1-555-123-4567" }
+  #   region_id { 1 }
+  #   resource_category_id { 1 }
+  #   description { "Fake Description" }
+  # end
+    factory :ticket do
+      name { 'Fake Ticket Name' }
+      description { 'Fake Ticket Description' }
+      phone { '+11234561234' }
+      closed { false }
+      resource_category 
+      region 
+    end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+
+
+# new does not save object to db, create will so create gives id
+# build_stbbed doesn't hit the db which is effective but it gives us an id still
+# when using more than one object, either need to instantiate one object and use it multiple times
+# or you can make multiple objects but you need to go to the factory and make the name unique
+# such as setting a name to a 'Fake Name + a time' so as they create, they will be milliseconds apart
+# sequence(:name { |n} "Fake resouce category #{n}" ")
+  
+    # let(ticket = Ticket.create!) (
+    #   name: 'Fake Ticket',
+    #   description: 'Fake Ticket Description',
+    #   phone: '+11234561234',
+    #   #organization_id: ??,
+    #   closed: false,
+    #   closed_at: nil,
+    #   resource_category: create(:resource_category),
+    #   region: create(:region)
+    #   )
+
   let(:ticket) { build(:ticket) }
 
   describe "ticket" do 
@@ -18,16 +38,19 @@ RSpec.describe Ticket, type: :model do
       expect(ticket.phone).to be_truthy
     end
     it "is invalid without a region ID" do
-      expect(ticket.region_id).to eq(1)
+      expect(ticket.region_id).to be_falsey
     end
     it "is invalid without a resource category ID" do 
-      expect(ticket.resource_category_id).to eq(1)
+      expect(ticket.resource_category_id).to be_falsey
     end
     it "has a description" do 
       expect(ticket.description).to be_truthy
     end
     it "is invalide if the name is less than 1 or more than 255" do 
       expect(ticket).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+    it "validates phone using phony_plausible" do
+      expect(ticket).to be_invalid
     end
   end
 
@@ -52,4 +75,22 @@ RSpec.describe Ticket, type: :model do
       end
     end
   end
+
+  # describe "scopes" do
+  #   describe "open" do 
+  #     # given there is ticket in db
+  #     open_ticket = Ticket.create!( name: 'Fake Ticket', description: 'Fake Ticket Description', phone: '+11234561234', organization: nil,
+  #       closed: false, closed_at: nil, resource_category: create(:resource_category), region: create(:region) )
+  #     closed_ticket = Ticket.create!(:closed)
+  #     results = Ticket.open
+  #     expect(results).to include(open_ticket)
+  #     expect(results).to_not include(closed_ticket)
+  #     # when retrieve open ticket
+  #     # should have list of tickets open
+  #     # and none that are not open
+  #       end
+  #     end
+  #   end
+  # end
+  
 end
