e23979f8adc96f4b2c2d10be26cc8f97ace8c3b5
Jonah Broyer <jkbsk8er@yahoo.com>
2022-02-13 16:03:43 -0800

    Rewrote validity and association tests References #22

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -8,43 +8,61 @@ RSpec.describe ResourceCategory, type: :model do
     it "is invalid without a name" do 
       expect(resource_category).to be_valid
     end
-    it "is invalid with a name greater than 255 char" do
-      expect(resource_category).to be_valid
+    it "cannot have a name length greater than 255" do
+      expect(resource_category).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+    it "is a unique name" do
+      expect(resource_category).to validate_uniqueness_of(:name).case_insensitive
     end
   end
-
 
   describe "associations" do
     it "has many tickets" do 
       expect(resource_category).to have_many(:tickets)
     end
+    it "has and belongs to many organizations" do
+      expect(resource_category).to have_and_belong_to_many(:organizations)
+    end
   end
 
-  
+  describe "scope" do
+    it "has a scope" do
+      resource_category = ResourceCategory.new
+      expect(resource_category).to respond_to(:active || :inactive)
+    end
+  end
+
   describe "methods" do
-    describe "unspecified" do
+    describe "::unspecified" do
       it "returns a resource category with the name 'Unspecified'" do
-        resource_category = build(:resource_category, name: 'Unspecified')
+        resource_category = ResourceCategory.unspecified
         expect(resource_category.name).to eq('Unspecified')
       end
     end
-    describe "activate" do
+    describe "#activate" do
       it "makes a region category active" do
-        expect(resource_category.activate).to be_truthy
+        resource_category.active = false
+        resource_category.activate
+        expect(resource_category.active).to be_truthy
       end
     end
-    describe "deactivate" do 
+    describe "#deactivate" do 
       it "makes a region category unactive" do 
-        expect(resource_category.deactivate).to be_truthy
+        resource_category.activate
+        expect(resource_category.active).to be_truthy
       end
     end
-    describe "inactive" do 
-      it "is an inactive resource category" do 
-        resource_category.active = true
-        expect(resource_category.inactive?).to eq(false)
+    describe "#inactive?" do 
+      it "is false when it is inactive" do 
+        resource_category.active = false
+        expect(resource_category.inactive?).to be_truthy
+      end
+      it "is true when it is inactive" do 
+        resource_category.active = false
+        expect(resource_category.inactive?).to be_truthy
       end
     end
-    describe "to_s" do
+    describe "#to_s" do
       it "has a string representation that is its name" do 
         name = resource_category.name
         expect(resource_category.to_s).to eq(name)
