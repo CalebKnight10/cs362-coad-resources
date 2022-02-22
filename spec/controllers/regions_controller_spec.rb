78b62ea90d7d32d867e28be4c69bcd4ed923aaa4
Jonah Broyer <jkbsk8er@yahoo.com>
2022-02-22 12:47:41 -0800

    tested for an organization user, need to fix creating a user References #25

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
-<<<<<<< HEAD
-=======
   describe "a non logged in person" do
     it "redirects to the sign in screen" do
       get :index
@@ -24,6 +22,30 @@ RSpec.describe RegionsController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
     end
   end
->>>>>>> 4b9b468ef006bfad73c4ddf7f1431b1f40a67a0e
+
+  describe "an organization user" do
+    it "redirects to the sign in screen" do
+      organization_user = create(:user)
+      organization_user.confirm!
+      organization_user.save
+      sign_in(organization_user)
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      get :new
+      expect(response).to redirect_to(dashboard_path)
+      post :create
+      expect(response).to redirect_to(dashboard_path)
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
 
 end
