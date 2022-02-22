df2a689021ff560c08c8fd2fa348d5ccf085fc8d
Caleb Knight <knightca@oregonstate.edu>
2022-02-22 11:45:25 -0800

    Added delete for resource_category

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       patch :update, params: {id: 'FAKE'}
       expect(response).to redirect_to(new_user_session_path)
-      # get :new
-      # expect(response).to redirect_to(new_user_session_path)
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
