98c9533983eba4ae742e792250b2e4a3c9bc949b
Caleb Knight <knightca@oregonstate.edu>
2022-03-01 10:53:10 -0800

    RCC :delete

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -34,6 +34,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       patch :update, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)      
     end
   end
 end
