c29cebd0aa45a5873f9e62c24f1934b2c321a211
Caleb Knight <knightca@oregonstate.edu>
2022-03-01 11:31:26 -0800

    Issue #30 :show

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe TicketsController, type: :controller do
 
   describe "a non logged in person" do 
     it "redirects to the sign in screen" do 
+      expect { get :show, params: {id: 'FAKE'} }.to raise_error
       delete :destroy, params: {id: 'FAKE'}
       expect(response).to redirect_to(dashboard_path)
     end
