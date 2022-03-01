3fe74ef2caef1d0e4955b48a706cf018e63d928f
Caleb Knight <knightca@oregonstate.edu>
2022-03-01 10:50:07 -0800

    RCC :create

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -28,6 +28,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(dashboard_path)
       get :new 
       expect(response).to redirect_to(dashboard_path)
+      get :create
+      expect(response).to redirect_to(dashboard_path)
     end
   end
 end
