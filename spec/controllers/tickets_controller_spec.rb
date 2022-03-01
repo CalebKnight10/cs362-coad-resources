f2f461d473bd08f6a48d52fbd51a874f4635dc34
Caleb Knight <knightca@oregonstate.edu>
2022-03-01 11:24:12 -0800

    First ticket test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  describe "a non logged in person" do 
+    it "redirects to the sign in screen" do 
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+
 end
