29e0aefadf241973413933945f28ddecf6c5e3e1
Jonah Broyer <jkbsk8er@yahoo.com>
2022-03-09 16:44:16 -0800

    Finished system test for admin creating a region References #32

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  describe "creating a region" do
+
+    scenario "admin creates a region successfully" do
+      admin = create(:user, :admin)
+      admin.confirm
+      log_in_as(admin)
+      visit "/dashboard"
+      click_on "Regions"
+      click_on "Add Region"
+      fill_in "Name", with: "Fake Region"
+      click_on "Add Region"
+      expect(page).to have_text "Region successfully created."
+    end
+  end
+
 end
