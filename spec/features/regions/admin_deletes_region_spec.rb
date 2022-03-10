bd95b5a600fe57f66737e243d17b30388ccded01
Jonah Broyer <jkbsk8er@yahoo.com>
2022-03-09 16:48:21 -0800

    Finished system test for admin deleting a region References #32

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  describe "deleting a region" do
+
+    scenario "admin deletes a region successfully" do
+      admin = create(:user, :admin)
+      admin.confirm
+      log_in_as(admin)
+      visit "/dashboard"
+      click_on "Regions"
+      click_on "Add Region"
+      region_name = "Fake Region"
+      fill_in "Name", with: region_name
+      click_on "Add Region"
+      click_on region_name
+      click_on "Delete"
+      expect(page).to have_text "Region #{region_name} was deleted."
+    end
+  end
+
 end
