7d5128c98198b1f3e34f2f952c085613117c77ff
Jonah Broyer <jkbsk8er@yahoo.com>
2022-03-12 16:28:25 -0800

    Finished system test for admin deleting a resource category References #33

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
 
+  describe "deleting a resource category" do
+
+    scenario "admin deletes a resource category successfully" do
+      admin = create(:user, :admin)
+      admin.confirm
+      log_in_as(admin)
+      resource_category = create(:resource_category)
+      visit "/resource_categories"
+      click_on resource_category.name
+      click_on "Delete"
+      expect(page).to have_text "Category #{resource_category.name} was deleted."
+    end
+  end
+
 end
